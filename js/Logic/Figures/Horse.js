8ff3120a3ed49526fc4bb707267ed1fd194d016b
Kirill Stepkin <kirillstyopkin@gmail.com>
2015-11-05 17:33:31 +0200

    fix horse moving

--- js/Logic/Figures/Horse.js
+++ js/Logic/Figures/Horse.js
@@ -8,32 +8,32 @@ class Horse extends Figure {
     move(posX, posY) {
 
     	if (8 - posX > 1) {
-	    	if ((posX - this.posX == 1  && posY - this.posY == 2) ||
-	    	    (posX - this.posX == -1 && posY - this.posY == 2)) {
+	    	if ((posY < 8 && posX - this.posX == 1 && posY - this.posY == 2) ||
+	    	    (posY < 8 && posX - this.posX == -1 && posY - this.posY == 2)) {
 	    		this.posX = posX;
 	    		this.posY = posY;
 	    	}
 		}
 
 		if (8 - posX > 0) {
-	    	if ((posX - this.posX == 2  && posY - this.posY == 1) ||
-	    	    (posX - this.posX == -2 && posY - this.posY == 1)) {
+	    	if ((posY < 8 && posX - this.posX == 2 && posY - this.posY == 1) ||
+	    	    (posY < 8 && posX - this.posX == -2 && posY - this.posY == 1)) {
 	    		this.posX = posX;
 	    		this.posY = posY;
 	    	}
 		}
     	
     	if (posX >= 1) {
-	    	if ((posX - this.posX == 2  && posY - this.posY == -1) ||
-	    	    (posX - this.posX == -2 && posY - this.posY == -1)) {
+	    	if ((posY >= 0 && posX - this.posX == 2 && posY - this.posY == -1) ||
+	    	    (posY >= 0 && posX - this.posX == -2 && posY - this.posY == -1)) {
 	    		this.posX = posX;
 	    		this.posY = posY;
 	    	}
 		}
     	
     	if (posX >= 0) {
-	    	if ((posX - this.posX == 1  && posY - this.posY == -2) ||
-	    	    (posX - this.posX == -1 && posY - this.posY == -2)) {
+	    	if ((posY >= 0 && posX - this.posX == 1 && posY - this.posY == -2) ||
+	    	    (posY >= 0 && posX - this.posX == -1 && posY - this.posY == -2)) {
 	    		this.posX = posX;
 	    		this.posY = posY;
 	    	}

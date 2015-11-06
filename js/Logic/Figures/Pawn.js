6ad85251ea011e2e9117374484c1d15195f4d225
Kirill Stepkin <kirillstyopkin@gmail.com>
2015-11-06 18:19:58 +0200

    fix

--- js/Logic/Figures/Pawn.js
+++ js/Logic/Figures/Pawn.js
@@ -9,6 +9,15 @@ class Pawn extends Figure {
 
     	//TODO
     	if (x - this.pos.x < 2 && y - this.pos.y < 2) {
+
+    		if (this.pos.x == x && y - this.pos.y == 1) {
+
+    		}
+
+    		if (this.pos.y == y && (x - this.pos.x == 1 || x - this.pos.x == -1)) {
+
+    		}
+
     		this.pos.x = x;
     		this.pos.y = y;
     	}

4204eac51b9f6d80fccfb9584d6a899c363685d0
Kirill Stepkin <kirillstyopkin@gmail.com>
2015-11-12 19:47:45 +0200

    fix

--- js/Render.js
+++ js/Render.js
@@ -16,15 +16,20 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 var data = new Array();
 
+var isWhite = false;
+
 for (var i = 7; i >= 0; i--) {
+	isWhite = !isWhite;
 	data[i] = {
-		index: 8-i, 
+		index: 8 - i, 
 		arr: new Array()
 	};
 	for (var j = 1; j < 9; j++) {
+		isWhite = !isWhite;
 		data[i].arr[j] = {
 			letter: letters[j-1],
-			num: 8-i
+			num: 8 - i,
+			color: isWhite ? 'white' : 'black'
 		};	
 	}
 }
